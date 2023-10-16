347a5613cde7b724475e7afa4166d83a2616a26a
Diego Ozuna <ozunad2002@gmail.com>
2023-10-15 19:10:45 -0500

    Added Splash and Icon on top of Katlyns App

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import BottomNavigationBar from './components/BottomNavigatorBar';
+
+
+// Changed it to class instead of a function -> N1
+export default class App extends React.Component{
+  
+  // ~~~~~ Bottom Navigation ~~~~~ //
+  constructor(props){
+    super(props);
+    this.state = {
+      // The text in the top right of the screen
+      screenText: "Welcome, User"
+    };
+  }
+  
+  // This changes the text for each button.
+  changeText = (text) => {
+    console.log(text + ' has been pressed')
+    this.setState({
+      screenText: text
+    });
+  };
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+  
+  render(){
+    return(
+
+      // ~~~~~ Bottom Navigation ~~~~~ //
+      <View style={styles.container}>
+        <StatusBar style='auto'/>
+        <BottomNavigationBar screenText={this.state.screenText} changeText={this.changeText}/>
+      </View>
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff', // Background color of the app.,
+    color: '#3ac78b',
+    alignItems: 'center'
+  }
 });
