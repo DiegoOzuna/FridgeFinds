667470ea38c4fce0eaae20c9056c20058107a7c0
Diego Ozuna <ozunad2002@gmail.com>
2023-10-21 02:41:49 -0500

    Removed instances of HomePage that was causing app to not bundle.

--- App.js
+++ App.js
@@ -13,7 +13,6 @@ const Stack = createNativeStackNavigator();
 
 //we need to import our made pages to add into stack
 import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
 import SignUp from './src/screens/SignUp';
 import ForgotPassword from './src/screens/ForgotPage';
 
@@ -30,7 +29,6 @@ export default function App() {
       <Stack.Navigator> 
         {/* we are making the top be the login, after authentication it will go elsewhere */}
         <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SignUp" component={SignUp} />
         {/*<Stack.Screen name="Forgot" component={ForgotPage} />*/}
 
