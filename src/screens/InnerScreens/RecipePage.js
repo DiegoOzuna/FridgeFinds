2e567a3d62a5a3737f040dcec7672cc7fb2b7f66
HectorJ-M <110851284+HectorJ-M@users.noreply.github.com>
2023-11-03 09:54:19 -0700

    Add files via upload
    Created the recipe page with a test to scan through a json file of Computer Science Languages

--- src/screens/InnerScreens/RecipePage.js
+++ src/screens/InnerScreens/RecipePage.js
@@ -1,26 +1,61 @@
 //place holder for area where user's current ingredients are
 //These imports are to have our bottomnavigation bar be on screen
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
-import { StyleSheet, View } from 'react-native';
 
+import { StyleSheet, TextInput, View, Keyboard, Button, Text,ActivityIndicator } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
 import BottomNavigationBar from '../components/BottomNavigatorBar';
+import Search from '../components/Search';
+import List from  '../components/List';
 // end of imports for bottomnavigation bar
 
-export class RecipePage extends React.Component {
+
+
+const RecipePage = () => {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [fakeData, setFakeData] = useState();
+
+  // get data from the fake api endpoint
+  useEffect(() => {
+    const getData = async () => {
+      const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+      );
+      const data = await apiResponse.json();
+      setFakeData(data);
+    };
+    getData();
+  }, []);
   
-  //render is what "draws" our navigation bar on screen
-  render(){
-    return(
-      // ~~~~~ Bottom Navigation ~~~~~ //
-      <View style={styles.container}>
-        <StatusBar style='auto'/>
-        <BottomNavigationBar/>
+  return (
+    <View style={styles.container}>
+      <StatusBar style='auto'/>
+      <View style={styles.topLeftContainer}>
+ 
       </View>
-      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-    );
-  }
-}
+ 
+      {!clicked && <Text style={styles.title}>Programming Languages</Text>}
+      <Search
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+      { (
+
+          <List
+            searchPhrase={searchPhrase}
+            data={fakeData}
+            setClicked={setClicked}
+          />
+
+      )}
+      <BottomNavigationBar/>
+    </View>
+  );
+};
 
 
 const styles = StyleSheet.create({
